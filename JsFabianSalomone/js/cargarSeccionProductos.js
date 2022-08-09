d3e47b26118ab3b83fb2f209eaf560b3558a8c87
Fabian Salomone <fabi4872@gmail.com>
2022-08-08 23:52:32 -0300

    Incorporacion de estilos

--- JsFabianSalomone/js/cargarSeccionProductos.js
+++ JsFabianSalomone/js/cargarSeccionProductos.js
@@ -16,8 +16,11 @@ class Producto{
 const productos = [];
 
 const producto1 = new Producto("kurzweil_sp2x", "Kurzweil", "Sp2x", "Piano eléctrico Kurzweil Sp2x, 88 teclas con peso piano martillo", 6, 1500, 200000, 10);
+const producto2 = new Producto("cort_x250_bk", "Cort", "X250-bk", "Guitarra eléctrica Cort X250-BK, cuerpo sólido con dos cutaways", 12, 700, 300000, 5);
+const producto3 = new Producto("knight_jbts_100", "Knight", "Jbts-100", "Saxo tenor Knight JBTS-100 Laqueado, llave de F#", 9, 2500, 400000, 2);
+const producto4 = new Producto("focusrite_scarlett_2i4", "Focusrite", "Scarlett 2i4", "Placa de sonido Focusrite Scarlett 2i4 USB", 12, 350, 100000, 25);
 
-productos.push(producto1);
+productos.push(producto1, producto2, producto3, producto4);
 
 let seccionProductos = document.getElementById("seccionProductos");
 
@@ -45,9 +48,9 @@ productos.forEach((producto) => {
         <h6 class="card-subtitle mb-2 text-muted cardProducto__modelo">Modelo ${producto.Modelo}</h6>
         <p class="card-text cardProducto__descripcion">${producto.Descripcion}</p>
         <h3 class="cardProducto__precio"><strong class="cardProducto__strongPrecio">ARS ${producto.Precio}</strong><h3/>
-        <p class="card-text cardProducto__info"><small class="text-muted">${producto.Cuotas} cuotas sin interés de <strong class="cardProducto__strong">ARS ${valorCuota}</strong></small></p>
-        <p class="card-text cardProducto__info"><small class="text-muted">Costo de envío: <strong class="cardProducto__strong">ARS ${producto.Envio}</strong></small></p>
-        <p class="card-text cardProducto__info"><small class="text-muted">Cantidad en stock: <strong class="cardProducto__strong">${producto.Stock}</strong></small></p>
+        <p class="card-text cardProducto__info"><i class="fa-solid fa-credit-card cardProducto__icon"></i><small class="text-muted">${producto.Cuotas} cuotas sin interés de <strong class="cardProducto__strong">ARS ${valorCuota}</strong></small></p>
+        <p class="card-text cardProducto__info"><i class="fa-solid fa-truck cardProducto__icon"></i><small class="text-muted">Costo de envío: <strong class="cardProducto__strong">ARS ${producto.Envio}</strong></small></p>
+        <p class="card-text cardProducto__info"><i class="fa-solid fa-business-time cardProducto__icon"></i><small class="text-muted">Cantidad en stock: <strong class="cardProducto__strong">${producto.Stock}</strong></small></p>
     `;
     cardBody.className = "card-body cardProducto__body";
 
